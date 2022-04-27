292406d85be21d0228ab0341787be8816110b3b3
Jeyson Alejandro Pereira <jpereiraf96@gmail.com>
2022-04-26 19:47:17 -0500

    practica mvc pt4
    se agregó pelota al canvas y una parte logica de movimiento a las barras

--- main.js
+++ main.js
@@ -18,6 +18,19 @@
 })();
 
 (function () {
+  self.Ball = function (x, y, radius, board) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.speed_y = 0;
+    this.speed_x = 3;
+    this.board = board;
+    board.ball = this;
+    this.kind = "circle";
+  };
+})();
+
+(function () {
   self.Bar = function (x, y, width, height, board) {
     this.x = x;
     this.y = y;
@@ -52,41 +65,65 @@
   };
 
   self.BoardView.prototype = {
+    clean: function () {
+      this.ctx.clearRect(0, 0, this.board.width, this.board.height);
+    },
     draw: function () {
       for (let i = this.board.elements.length - 1; i >= 0; i--) {
         let el = this.board.elements[i];
         draw(this.ctx, el);
       }
     },
+    play: function () {
+      this.clean();
+      this.draw();
+    },
   };
+
   function draw(ctx, element) {
-    if (element !== null && element.hasOwnProperty("kind")) {
-      switch (element.kind) {
-        case "rectangle":
-          ctx.fillRect(element.x, element.y, element.width, element.height);
-          break;
-      }
+    // if (element !== null && element.hasOwnProperty("kind")) {
+    switch (element.kind) {
+      case "rectangle":
+        ctx.fillRect(element.x, element.y, element.width, element.height);
+        break;
+      case "circle":
+        ctx.arc(element.x, element.y, element.radius, 0, 7);
+        ctx.fill();
+        ctx.closePath();
+        break;
     }
   }
 })();
 
 let board = new Board(800, 400);
-var bar = new Bar(20, 100, 40, 100, board);
-var bar = new Bar(735, 100, 40, 100, board);
+let bar = new Bar(20, 100, 40, 100, board);
+let bar2 = new Bar(735, 100, 40, 100, board); //Renombrada para identificar segunda barra
 let canvas = document.getElementById("canvas");
 let board_view = new BoardView(canvas, board);
+let ball = new Ball(350, 100, 10, board);
 
 document.addEventListener("keydown", function (event) {
+  event.preventDefault();
+  // Manejo de movimiento mediante flecha arriba y abajo
   if (event.keyCode == 38) {
     bar.up();
   } else if (event.keyCode == 40) {
     bar.down();
+  } else if (event.keyCode === 87) {
+    // Movimiento mediante W
+    bar2.up();
+  } else if (event.keyCode === 83) {
+    // Movimiento mediante S
+    bar2.down();
   }
-  console.log("" + bar);
 });
 
-window.addEventListener("load", main);
-
-function main() {
-  board_view.draw();
+// window.addEventListener("load", main);
+window.requestAnimationFrame(controller);
+// funcion renombrada anteriormente main -> ahora controlador
+function controller() {
+  board_view.play();
+  // board_view.clean();
+  // board_view.draw()
+  window.requestAnimationFrame(controller);
 }
