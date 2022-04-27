9525c438f5220351bbb887c24e5ee2f47d1522d3
Jeyson Alejandro Pereira <jpereiraf96@gmail.com>
2022-04-26 20:07:52 -0500

    practica mvc pt5
    se agregó movimiento a la pelota, se mejoro control en la funcion play

--- main.js
+++ main.js
@@ -10,7 +10,9 @@
 
   self.Board.prototype = {
     get elements() {
-      let elements = this.bars;
+      var elements = this.bars.map(function (bar) {
+        return bar;
+      });
       elements.push(this.ball);
       return elements;
     },
@@ -25,8 +27,16 @@
     this.speed_y = 0;
     this.speed_x = 3;
     this.board = board;
+    this.direction = -1;
     board.ball = this;
     this.kind = "circle";
+  };
+
+  self.Ball.prototype = {
+    move: function () {
+      this.x = this.x + this.speed_x * this.direction;
+      this.y += this.speed_y;
+    },
   };
 })();
 
@@ -75,8 +85,11 @@
       }
     },
     play: function () {
-      this.clean();
-      this.draw();
+      if (this.board.playing) {
+        this.clean();
+        this.draw();
+        this.board.ball.move();
+      }
     },
   };
 
@@ -87,6 +100,7 @@
         ctx.fillRect(element.x, element.y, element.width, element.height);
         break;
       case "circle":
+        ctx.beginPath();
         ctx.arc(element.x, element.y, element.radius, 0, 7);
         ctx.fill();
         ctx.closePath();
@@ -97,7 +111,7 @@
 
 let board = new Board(800, 400);
 let bar = new Bar(20, 100, 40, 100, board);
-let bar2 = new Bar(735, 100, 40, 100, board); //Renombrada para identificar segunda barra
+let bar2 = new Bar(735, 100, 40, 100, board);
 let canvas = document.getElementById("canvas");
 let board_view = new BoardView(canvas, board);
 let ball = new Ball(350, 100, 10, board);
@@ -106,21 +120,34 @@ document.addEventListener("keydown", function (event) {
   event.preventDefault();
   // Manejo de movimiento mediante flecha arriba y abajo
   if (event.keyCode == 38) {
+    event.preventDefault();
     bar.up();
   } else if (event.keyCode == 40) {
+    event.preventDefault();
     bar.down();
   } else if (event.keyCode === 87) {
+    event.preventDefault();
     // Movimiento mediante W
     bar2.up();
   } else if (event.keyCode === 83) {
+    event.preventDefault();
     // Movimiento mediante S
     bar2.down();
+  } else if (event.keyCode === 32) {
+    event.preventDefault();
+    board.playing = !board.playing;
   }
 });
 
-// window.addEventListener("load", main);
+board_view.draw(); // Dibujar canvas al cargar pagina
+
 window.requestAnimationFrame(controller);
-// funcion renombrada anteriormente main -> ahora controlador
+
+setTimeout(function () {
+  ball.direction = -1;
+}, 4000);
+
+// controlador del juego
 function controller() {
   board_view.play();
   // board_view.clean();
